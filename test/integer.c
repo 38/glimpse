aa26e4224c76fa3d580cf9e5020642c67167c23f
ghost <ghost@ghost-MacBookAir.(none)>
2013-04-11 21:47:25 +0800

    add test for int

--- test/integer.c
+++ test/integer.c
@@ -1,11 +1,22 @@
 #include <pluginloader.h>
 #include <TypeAPI.h>
+#include <integer/integer.h>
 int main()
 {
 	Glimpse_TypeAPI_init();
 	glimpse_pluginloader_path[0] = ".";
 	glimpse_pluginloader_path[1] = NULL;
 	glimpse_pluginloader_load_plugin("integer");
+	GlimpseTypeDesc_t* td = glimpse_typesystem_newdesc(sizeof(GlimpseIntegerProperties_t));
+	GlimpseIntegerProperties_t* properties = (GlimpseIntegerProperties_t*)td->properties;
+	td->group = "integer";
+	td->flag = 0;
+	properties->Size = GlimpseInteger32;
+	properties->Signed = 1;
+	properties->Representation = GlimpseIntegerHex;
+	properties->Leading = NULL;
+	GlimpseTypeHandler_t handler = {};
+	glimpse_typesystem_query(td, &handler);
 	return 0;
 
 }
