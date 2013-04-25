cdf919fdf8813f5d4d4b597d4bea747ee525b969
Hao Hou <ghost89413@gmail.com>
2013-04-25 11:40:59 +0800

    add license

--- include/glimpse/future.h
+++ include/glimpse/future.h
@@ -1,3 +1,22 @@
+/* future.h -   
+ *
+ * Copyright 2013 Hao Hou <ghost89413@gmail.com>
+ * 
+ * This file is part of Glimpse, a fast, flexible key-value scanner.
+ * 
+ * Glimpse is free software: you can redistribute it and/or modify it under the terms 
+ * of the GNU General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Glimpse is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+ * PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Glimpse. 
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+	
 #ifndef __FUTURE_H__
 #define __FUTURE_H__
 /* 
@@ -21,7 +40,6 @@
 #ifndef NO_DEFAULT_FEATURES
 #	define LAZY_INSTANCE	/* makes the data instance object does not free the memery after its death, 
 							 * hoping this will reduce the time of calling instance new/free */
-#	define THREAD_SAFE			/* make Glimpse thread-safe, but might slow the parser down */
 #endif
 /*
  * deprecated features is features that does not use any more 
--- utils/cli.c
+++ utils/cli.c
@@ -1,3 +1,22 @@
+/* cli.c -   
+ *
+ * Copyright 2013 Hao Hou <ghost89413@gmail.com>
+ * 
+ * This file is part of Glimpse, a fast, flexible key-value scanner.
+ * 
+ * Glimpse is free software: you can redistribute it and/or modify it under the terms 
+ * of the GNU General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Glimpse is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+ * PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Glimpse. 
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+	
 #include <malloc.h>
 #include <string.h>
 #include <stdio.h>
@@ -545,7 +564,7 @@ int main(int argc, char** argv)
 #ifdef GLIMPSE_DEBUG
 	api_init();
 #endif
-	glimpse_pluginloader_path[0] = ".";
+	glimpse_pluginloader_path[0] = GLIMPSE_CLI_DEFAULT_PATH;
 	glimpse_pluginloader_path[1] = NULL;
 	fp_input = stdin;
 	fp_output = stdout;
